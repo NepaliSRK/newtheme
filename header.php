d2b1f5831c02e6b2a52173d21563944cc4ba76ef
NepaliSRK <nepalisrk@gmail.com>
2021-11-18 13:54:56 +0545

    first change

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     
+        <link rel="stylesheet" href="style.css">
+        <?php wp_head();?>
+</head>
+    <body <?php body_class();?>>
+
+<header class="sticky-top">
+
+     <div class="manage">
+
+     <?php wp_nav_menu(
+
+                        array(
+                               'theme_location' => 'top-menu',
+                                'menu_class' => 'navigation'
+                                )
+                        );?>
+
+                        </div>
+
+</header>
