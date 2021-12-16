ffa36d8bac15675c6cc02a5df328842ea2483d31
NepaliSRK <nepalisrk@gmail.com>
2021-12-16 14:22:58 +0545

    Ajax added custom post

--- to-do/post.php
+++ to-do/post.php
@@ -1,37 +1,15 @@
 <?php
 
-/*$my_post = array(
-  'post_title'    => wp_strip_all_tags( $_POST['post_title'] ),
-  'post_content'  => $_POST['post_content'],
-  'post_status'   => 'publish',
-  'post_author'   => 1,
-  'post_category' => array( 8,39 )
-);
+function news_add_post()
+{
+    $my_post = array(
+        'post_title'    => wp_strip_all_tags( $_POST['name'] ),
+        'post_content'  => $_POST['message'],
+        'post_status'   => 'publish',
+    );
 
-// Insert the post into the database
-wp_insert_post( $my_post );*/
-
-
-function custom_post_type() {
-  
-  $args = array(
-      'labels'      => array(
-          'name'          => __( 'news' ),
-          'singular_name' => __( 'News' )
-      ),
-      'public'      => true,
-      'has_archive' => true,
-      'rewrite'     => array( 'slug' => 'news' ),
-  );
-  
-  register_post_type( 'news', $args );
-}
-
-add_action( 'init', 'custom_post_type' );
-
-function news_add_post() {
+    wp_insert_post( $my_post );
 
 }
 
-add_action( "wp_ajax_news_add_post", "news_add_post" );
-
+add_action("wp_ajax_news_add_post", "news_add_post");
