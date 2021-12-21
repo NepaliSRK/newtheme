a28ae0a80387a5dd6bf2232f6f1ec2bc4183cd77
NepaliSRK <nepalisrk@gmail.com>
2021-12-21 14:23:31 +0545

    index page edited

--- index.php
+++ index.php
@@ -28,29 +28,13 @@ get_header(); ?>
         <div id="primary" class="content-area">
             <div id="content" class="site-content" role="main">
 
-                <?php
-                if ( have_posts() ) :
-                    // Start the Loop.
-                    while ( have_posts() ) :
-                        the_post();
+                <h1><?php the_title(); ?></h1>
 
-                        /*
-                         * Include the post format-specific template for the content. If you want
-                         * to use this in a child theme, then include a file called content-___.php
-                         * (where ___ is the post format) and that will be used instead.
-                         */
-                        get_template_part( 'content', get_post_format() );
+                <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-                    endwhile;
-                    // Previous/next post navigation.
-                    newtheme_paging_nav();
+                    <?php the_content(); ?>
 
-                else :
-                    // If no content, include the "No posts found" template.
-                    get_template_part( 'content', 'none' );
-
-                endif;
-                ?>
+                <?php endwhile; endif; ?>
 
             </div><!-- #content -->
         </div><!-- #primary -->
